78fac47894e5fb173412b9ee37b3b402d9a8ab12
Nina Dang <ninadangt@gmail.com>
2013-11-25 13:18:31 -0800

    latest

--- js/createBasket.js
+++ js/createBasket.js
@@ -1,24 +1,9 @@
-//Global variable Basket
+// Global variable Basket
 var pizza = com.dawgpizza.menu.pizzas;
+var desserts = com.dawgpizza.menu.desserts;
+var drinks = com.dawgpizza.menu.drinks;
 var basket = new Array();
 
-// click to order function
-$(function() {
-    $(".order").click(function(event) {
-        alert(event.target.id);
-        var id = event.target.id.split("=");
-        var item = id[0];
-        var type = id[1];
-        var p = makeBasketItem(item, type); 
-        basket.push(p);  //
-        $("#cart-container").append(name(p)); 
-        $("#total-price").html(total());
-    });    
-});
-
-function name(p) {
-    return p.name + "            " + p.cost;
-}
 
 function total() {
     var total = 0;
@@ -27,31 +12,153 @@ function total() {
         total += current.cost;
     }
     total += total * .095;
-    return total;
+    return total.toFixed(2);
+}
+
+function subTotal() {
+    var total = 0;
+    for(var idx = 0; idx < basket.length; ++idx){
+        var current = basket[idx];
+        total += current.cost;
+    }
+    return total.toFixed(2);
+}
+
+function tax(total) {
+    total = total * .095;
+    return total.toFixed(2);
+}
+
+function name(p) {
+    return p.name + "    " + "($" + p.cost + ")" + "<br>";
 }
 
 // Basket items
 function makeBasketItem(item, type) {
+    var foodType;
+    if(type == 'de'){
+        foodType = desserts;
+    }else if(type == 'dr'){
+        foodType = drinks;
+    }else{
+        foodType = pizza;
+    }
+
     var label;
-    for(var idx = 0; idx < pizza.length; ++idx){
-        var current = pizza[idx];
+    for(var idx = 0; idx < foodType.length; ++idx){
+        var current = foodType[idx];
         if (current.name == item){
-            label = pizza[idx];
+            label = foodType[idx];
         }
     }
 
-    var price;
-    if(type == 's'){
-        price = 0;
-    }else if(type == 'm'){
-        price = 1;
-    }else{
-        price = 2;
-    }
+    if (foodType == pizza){
+        var price;
+        if(type == 's'){
+            price = 0;
+        }else if(type == 'm'){
+            price = 1;
+        }else{
+            price = 2;
+        }
 
-    var obj = {
-        name: label.name,
-        cost: label.prices[price]
+        var obj = {
+            name: label.name,
+            cost: label.prices[price]
+        }
+
+    }else{
+        var obj = {
+            name: label.name,
+            cost: label.price
+        }
     }
     return obj;
 }
+
+// postCart()
+// posts the cart model to the server using
+// the supplied HTML form
+// parameters are:
+//  - cart (object) reference to the cart model
+//  - cartForm (jQuery object) reference to the HTML form
+//
+function postCart(cart, cartForm) {
+    //find the input in the form that has the name of 'cart'    
+    //and set it's value to a JSON representation of the cart model
+    cartForm.find('input[name="cart"]').val(JSON.stringify(cart));
+
+    //submit the form--this will navigate to an order confirmation page
+    cartForm.submit();
+
+} //postCart()
+
+//On-Ready
+$(function() { 
+    var cart = {
+        name: null,
+        address1: null,
+        zip: null,
+        phone: null,
+        items: [] 
+    }; //cart data
+
+    $(".order").click(function(event) {
+        var newCartItem = {
+            type: this.getAttribute('pizza.type'),
+            name: this.getAttribute('pizza.name'),
+            size: this.getAttribute('pizza.size'),
+            price: this.getAttribute('pizza.price')
+        };
+        cart.items.push(newCartItem);
+
+        //Display in shopping cart
+        var id = event.target.id.split("=");
+        console.log(id);
+        var item = id[0];
+        var type = id[1];
+        var p = makeBasketItem(item, type);
+        basket.push(p);  
+        $("#cart-container").append(name(p));
+        $("#sub-price").html(subTotal());
+        $("#tax-price").html(tax(total()));
+        $("#total-price").html(total());   
+    });
+
+    //Validation
+    $('.signup-form').submit(function(){
+        var signupForm = $(this);
+        var addr1Input = signupForm.find('input[name="addr-1"]');
+        var addr1Value = addr1Input.val();
+        if(addr1Value.length > 0){
+                var zipInput = signupForm.find('input[name="zip"]');
+                var zipValue = zipInput.val();
+                if(zipValue.length == 0){
+                        alert('Please provide a zip code.');
+                        return false;
+                }
+        }
+        var min = total();
+        if(min < 20){
+            alert('Please place a minimum order of $20');
+            return false;
+        }
+    }); //Submit signup
+
+    $('#empty').click(function(){
+        basket = new Array();
+        location.reload();
+    });
+
+    $('#place-order').click(function(){
+        var form =  $(this);
+        
+        cart.name = form.find('input[name="fl-name"]').val();
+        cart.address1 = form.find('input[name="addr-1"]').val();
+        cart.zip = form.find('input[name="zip"]').val();
+        cart.phone = form.find('input[name="phone"]').val();
+        
+        postCart(cart, $('#the-master-form'));
+        
+    });
+});
--- js/menu-data.js
+++ js/menu-data.js
@@ -6,17 +6,17 @@ if ($("title").html() == "Menu") {
 	$(function(){
 		//console.log("ready!");
 		pizzaRender(com.dawgpizza.menu.pizzas);
-		dRender(com.dawgpizza.menu.drinks, '.drinks', 'dr');
-		dRender(com.dawgpizza.menu.desserts, '.desserts', 'de');
+		dRender(com.dawgpizza.menu.drinks);
+		dRender(com.dawgpizza.menu.desserts);
 	}); //on ready function
 }
 if ($("title").html() == "Order") {
 	$(function() {
 		renderPizzaOrder(com.dawgpizza.menu.pizzas);
+		renderDOrder(com.dawgpizza.menu.drinks, '.drinks', 'dr');
+		renderDOrder(com.dawgpizza.menu.desserts, '.desserts', 'de');
 	});
 }
-
-
 
 function pizzaRender(entries){
 	var instance;
@@ -35,7 +35,6 @@ function pizzaRender(entries){
 	}); //for each pizza, input name, description, and price
 }
 
-
 function dRender(entries, type, suffix){
 	var instance;
 	var template = $(type);
@@ -44,12 +43,10 @@ function dRender(entries, type, suffix){
 	    instance = template.clone();
 	   	instance.find('.name-' + suffix).html(this.name);
 	    instance.find('.price-' + suffix).html(this.price);
-	    
 	    instance.removeClass('template');
         food.append(instance);
 
 	}); //for each drink, input name and price
-	
 }
 
 function renderPizzaOrder(entries) {
@@ -60,10 +57,36 @@ function renderPizzaOrder(entries) {
 	    instance = template.clone();
 	   	instance.find('.name').html(this.name);
 	    instance.find('.desc').html(this.description);
-	    instance.find('.pizza-s').html('<a class="order" id="' + this.name + '=s">' + this.prices[0] + '</a>');
-		instance.find('.pizza-m').html('<a class="order" id="' + this.name + '=m">' + this.prices[1] + '</a>');
-		instance.find('.pizza-l').html('<a class="order" id="' + this.name + '=l">' + this.prices[2] + '</a>');	    
+	    
+	    var tempSmall = instance.find('.pizza-s');
+	    tempSmall.html('<a data-size="small" class="order" id="' + this.name + '=s">' + this.prices[0] + '</a>');
+	    tempSmall.find("a").attr('data-price', this.prices[0]);
+
+	   	var tempMedium = instance.find('.pizza-m');
+	    tempMedium.html('<a data-size="medium" class="order" id="' + this.name + '=m">' + this.prices[1] + '</a>');
+	    tempMedium.find("a").attr('data-price', this.prices[1]);
+
+	    var tempLarge = instance.find('.pizza-l');
+	    tempLarge.html('<a data-size="large" class="order" id="' + this.name + '=l">' + this.prices[2] + '</a>');
+	    tempLarge.find("a").attr('data-price', this.prices[2]);
+
+		instance.find('a').attr('data-name', this.name);
+		instance.find('a').attr('data-type', this.type);
 	    instance.removeClass('template');
         food.append(instance);
 	});
 }
+
+function renderDOrder(entries, type, suffix){
+	var instance;
+	var template = $(type);
+	var food = $('.food-' + suffix);
+	$.each(entries, function(){
+	    instance = template.clone();
+	   	instance.find('.name-' + suffix).html(this.name);
+	    instance.find('.price-' + suffix).html('<a class="order" id="' + this.name + '=' + suffix + '">' + this.price + '</a>');
+	    instance.removeClass('template');
+        food.append(instance);
+
+	}); 
+} 
